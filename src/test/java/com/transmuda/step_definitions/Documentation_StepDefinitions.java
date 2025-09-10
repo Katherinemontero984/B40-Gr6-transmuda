b8f8407a8283e428fb26d793a26a1447b2e28bd6
HatefAbedini <habedini1@student.gsu.edu>
2025-09-10 19:58:28 -0400

    I added some codes in my step def. class

--- src/test/java/com/transmuda/step_definitions/Documentation_StepDefinitions.java
+++ src/test/java/com/transmuda/step_definitions/Documentation_StepDefinitions.java
@@ -1,11 +1,17 @@
 package com.transmuda.step_definitions;
 
 import com.transmuda.pages.DocumentationPage;
+import com.transmuda.utilities.BrowserUtils;
+import com.transmuda.utilities.ConfigurationReader;
 import com.transmuda.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.java.eo.Do;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.Dictionary;
@@ -15,34 +21,48 @@ public class Documentation_StepDefinitions
 
     DocumentationPage documentationPage = new DocumentationPage();
 
+    String mainWindow;
+
     @Given("user is on the home page")
     public void user_is_on_the_home_page()
     {
-        Driver.getDriver().get("https://qa.transmuda.com/user/login");
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        BrowserUtils.waitForPageToLoad(5);
+        mainWindow = Driver.getDriver().getWindowHandle(); // Storing the current window
+
     }
-    @When("user enter the {string} information")
-    public void user_enter_the_information(String userType)
+
+
+    @When("the user is logged in as {string}")
+    public void theUserIsLoggedInAs(String userType)
     {
 
     }
 
-    @Then("user should be able to login")
-    public void userShouldBeAbleToLogin()
-    {
 
-    }
-
-
-    @When("user click on the {string} icon")
+    @And("user click on the {string} icon")
     public void user_click_on_the_icon(String questionMark)
     {
-
+        // I'm waiting to click
+        BrowserUtils.waitForClickablility(documentationPage.questionMarkIcon,5).click();
     }
-    @Then("user should be redirected to {string}")
-    public void user_should_be_redirected_to(String expectedURL)
+
+
+    @Then("a new window should open with URL {string}")
+    public void aNewWindowShouldOpenWithURL(String expectedUrl)
     {
+        //waiting for a second window to open
+        BrowserUtils.switchToWindow(mainWindow);
 
+        String actual = Driver.getDriver().getCurrentUrl();
+        String expected = "https://doc.oroinc.com/";
+
+        Assert.assertEquals(expected, actual);
     }
+
+
+
+
 
 
 }

11edeeab31bc4f3896b54937cdaac248c0f8849d
DinaraBaizakova <bdf.1801@gmail.com>
2025-09-09 22:03:43 -0700

    I created ReccuringCalendarEvent StepDefs

---
+++ src/test/java/com/transmuda/step_definitions/RecurringCalendarEventStepDefs.java
@@ -0,0 +1,65 @@
+package com.transmuda.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RecurringCalendarEventStepDefs {
+
+    @Given("User navigates to the {string} section")
+    public void user_navigates_to_the_section(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("User opens the {string} page")
+    public void user_opens_the_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User cliks at the {string} button")
+    public void user_cliks_at_the_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User cliks at the {string} input field")
+    public void user_cliks_at_the_input_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("User is on the {string} page")
+    public void user_is_on_the_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User has opened the {string} section")
+    public void user_has_opened_the_section(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The default value of {string} should be {string}")
+    public void the_default_value_of_should_be(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @When("User clears the {string} input field")
+    public void user_clears_the_input_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User clicks anywhere on the page")
+    public void user_clicks_anywhere_on_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The error message {string} should be displayed")
+    public void the_error_message_should_be_displayed(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}

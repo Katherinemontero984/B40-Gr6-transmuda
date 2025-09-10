b7cb7867876b2df07b477596fda9912aa834dcdc
HatefAbedini <habedini1@student.gsu.edu>
2025-09-10 19:59:01 -0400

    I added some codes in my Doc. page class

--- src/test/java/com/transmuda/pages/DocumentationPage.java
+++ src/test/java/com/transmuda/pages/DocumentationPage.java
@@ -2,6 +2,7 @@ package com.transmuda.pages;
 
 import com.transmuda.utilities.ConfigurationReader;
 import com.transmuda.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -9,33 +10,12 @@ import org.openqa.selenium.support.PageFactory;
 public class DocumentationPage extends BasePage
 {
 
-    @FindBy(id ="prependedInput")
-    public WebElement username;
+    @FindBy(css = "i.fa-question-circle")
+    public WebElement questionMarkIcon;
 
-    @FindBy(id ="prependedInput2")
-    public WebElement password;
-
-    @FindBy(id = "_submit")
-    public WebElement loginButton;
-
-    public void loginAsTruckDriver()
+    public void clickQuestionMarkIcon()
     {
-        username.sendKeys(ConfigurationReader.getProperty("driver_username"));
-        password.sendKeys(ConfigurationReader.getProperty("driver_password"));
-        loginButton.click();
-    }
-
-    public void loginAsSalesManager() {
-        username.sendKeys(ConfigurationReader.getProperty("driver_username"));
-        password.sendKeys(ConfigurationReader.getProperty("driver_password"));
-        loginButton.click();
-    }
-
-    public void loginAsStoreManager() {
-        username.sendKeys(ConfigurationReader.getProperty("driver_username"));
-        password.sendKeys(ConfigurationReader.getProperty("driver_password"));
-        loginButton.click();
-
+        questionMarkIcon.click();
     }
 }
 

ee09826f3bd245e78f30df807a9ef2ad816e374b
bpalma91 <boryspalma@gmail.com>
2025-09-11 18:30:48 -0400

    Created feature and StepDef for B40GRP06-128 -Borys

---
+++ src/test/java/com/transmuda/step_definitions/VehiclesModelStepDef.java
@@ -0,0 +1,23 @@
+package com.transmuda.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class VehiclesModelStepDef {
+    @Given("navigates to the fleet option on the menu")
+    public void navigates_to_the_fleet_option_on_the_menu() {
+
+    }
+    @Then("clicks on the Vehicles Model Option")
+    public void clicks_on_the_vehicles_model_option() {
+
+    }
+    @Then("user should be able to see the {int} expected columns for every vehcihcle loaded into the system")
+    public void user_should_be_able_to_see_the_expected_columns_for_every_vehcihcle_loaded_into_the_system(Integer int1) {
+
+    }
+    @Then("driver should see “You do not have permission to perform this action.” if they try to access the same page")
+    public void driver_should_see_you_do_not_have_permission_to_perform_this_action_if_they_try_to_access_the_same_page() {
+
+    }
+}

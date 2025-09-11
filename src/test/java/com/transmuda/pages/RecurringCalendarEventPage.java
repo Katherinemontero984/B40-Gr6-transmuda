314e0e435c5357eaeda97c8dd5004b58c5ae7c29
DinaraBaizakova <bdf.1801@gmail.com>
2025-09-10 23:17:18 -0700

    I created ReccuringCalendarEvent Page

--- src/test/java/com/transmuda/pages/RecurringCalendarEventPage.java
+++ src/test/java/com/transmuda/pages/RecurringCalendarEventPage.java
@@ -1,24 +1,23 @@
 package com.transmuda.pages;
 
+import com.transmuda.utilities.BrowserUtils;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.devtools.idealized.Events;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class RecurringCalendarEventPage extends BasePage {
-
-
-
-    @Override
-    public void navigateToModule(String tab, String module) {
-        super.navigateToModule("Activities", "Calendar Events");
-    }
-
 
 
 @FindBy(xpath = "//div[@class='pull-right title-buttons-container']")
     public WebElement createCalendarEventButton;
 
-    @FindBy(name="oro_calendar_event_form[allDay]")
+    @FindBy(xpath = "//input[@data-name='recurrence-repeat']")
     public WebElement repeatInputField;
 
     @FindBy(xpath = "//input[@data-related-field='interval'][1]")
@@ -26,5 +25,17 @@ public class RecurringCalendarEventPage extends BasePage {
 
     @FindBy (xpath = "//span[text()='This value should not be blank.']")
     public WebElement valueBlank;
+
+    @FindBy(xpath ="(//h1)[2]")
+    public WebElement justClick;
+
+
+
+    public void waitForRepeatEveryField() {
+        BrowserUtils.waitForVisibility(repeatEveryDaysField, 10);
+        BrowserUtils.waitForClickablility(repeatEveryDaysField, 10);
+
+    }
+
 
 }
